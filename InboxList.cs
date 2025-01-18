4a26e6e55f7b074bd734fa504d6732a66df77904
Alec Bargher <abargher@uchicago.edu>
2025-01-18 13:59:50 -0600

    alec: add c-sharp files and empty InboxList script

---
+++ InboxList.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class InboxList : ItemList
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// set multiline mode here
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
