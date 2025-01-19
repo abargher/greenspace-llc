5d1871bb54df21822835541d98ed12af8f7fafea
Alec Bargher <abargher@uchicago.edu>
2025-01-19 15:18:22 -0600

    alec: fix broken call bc of racing signals

--- scripts/OfficePcView.cs
+++ scripts/OfficePcView.cs
@@ -7,8 +7,7 @@ public partial class OfficePcView : Control
 	public override void _Ready()
 	{
         metricsHud = GetNode<MetricsHud>("/root/Gameplay/HUDManager/MetricsHUD"); 
-        metricsHud.OnChangeSEO(10,5,2);
-        
+		metricsHud.CallDeferred(nameof(MetricsHud.OnChangeSEO), 10, 5, 2);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
