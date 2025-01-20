6c24fe9c71f4f09e6d2c0acf8906c907ee8cbc56
Alec Bargher <abargher@uchicago.edu>
2025-01-20 12:41:47 -0600

    alec: water cooler dialogue coming along

--- scripts/WaterCooler.cs
+++ scripts/WaterCooler.cs
@@ -14,13 +14,27 @@ public partial class WaterCooler : Node
 	[Export]
 	Array<Array<string>> answers;
 	[Export]
-	string retort;
+	Array<string> retorts;
+
+	Timer questionTimer;
+	Timer answersTimer;
+	Button returnButton;
+	
 
 	[Export]
 	RichTextLabel questionLabel;
 	[Export]
 	Array<RichTextLabel> answerButtons;
 
+	[Export]
+	VBoxContainer answerContainer;
+
+
+
+	private string FormatText(string text)
+	{
+		return $"[center]{text}[/center]";
+	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -31,9 +45,18 @@ public partial class WaterCooler : Node
 		// currentDayIndex = Math.Max(gameplay.currentDay - 1, NUM_CONVERSATION_DAYS);
 		currentDayIndex = 0;
 
-		
+		questionTimer = GetNode<Timer>("QuestionTimer");
+		answersTimer = GetNode<Timer>("AnswersTimer");
+		returnButton = GetNode<Button>("ReturnButton");
 
 
+		questionLabel.Text = FormatText(questions[currentDayIndex]);
+		GD.Print(questionLabel.Text);
+		for (int i = 0; i < answerButtons.Count; i++)
+		{
+			answerButtons[i].Text = FormatText(answers[currentDayIndex][i]);
+		}
+		questionLabel.Visible = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,4 +64,25 @@ public partial class WaterCooler : Node
 	{
 
 	}
+
+	public void OnQuestionTimerTimeout()
+	{
+		answerContainer.Visible = true;
+		for (int i = 0; i < answerButtons.Count; i++)
+		{
+			answerButtons[i].Visible = true;
+		}
+		answersTimer.Start();
+	}
+
+	public void OnAnswerTimerTimeout()
+	{
+		returnButton.Visible = true;
+	}
+
+	public void OnReturnButtonClick()
+	{
+		sceneManager.SwapScenes("res://scenes/office_pc_view.tscn", GetNode<Gameplay>("/root/Gameplay"), this, "fade_to_black");
+	}
+
 }
