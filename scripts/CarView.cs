ac5e796f54e9146ce5911cb4710c4ad12b418819
Alec Bargher <abargher@uchicago.edu>
2025-01-19 23:17:27 -0600

    alec: driving works

--- scripts/CarView.cs
+++ scripts/CarView.cs
@@ -7,9 +7,22 @@ public partial class CarView : Control
 
 	[Export]
 	public AudioStreamPlayer backgroundPlayer;
+	[Export]
+	public AudioStreamPlayer songPlayer;
+	[Export]
+	public AudioStreamPlayer dialoguePlayer;
+	[Export]
+	public TextureRect backgroundImage;
+	[Export]
+	public TextureRect wheel;
+
+	const float minWheelRotation = -30;
+	const float maxWheelRotation = 30;
+
 	AudioStreamWav radioSong;
 	double timeElapsed = 0;
-	const double speed = 1.0;
+	const float speed = 1.0f;
+	const float rotationSpeed = 0.3f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,14 +31,27 @@ public partial class CarView : Control
 		
 		// EmitSignal(Gameplay.SignalName.UpdateBackgroundTrack, $"res://audio/music/radio/song{trackNum}.wav");
 		EmitSignal(Gameplay.SignalName.UpdateBackgroundTrack, null);
-		
+		GD.Print(wheel.RotationDegrees);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		timeElapsed += delta;
-		Math.Sin(timeElapsed);
+		if (Input.IsActionPressed("ui_left"))
+		{
+			wheel.RotationDegrees = Mathf.Clamp(wheel.RotationDegrees - (rotationSpeed * 2f), minWheelRotation, maxWheelRotation);
+			backgroundImage.Position += new Vector2(speed, 0);
+		}
+		else if (Input.IsActionPressed("ui_right"))
+		{
+			wheel.RotationDegrees = Mathf.Clamp(wheel.RotationDegrees + (rotationSpeed * 2f), minWheelRotation, maxWheelRotation);
+			backgroundImage.Position -= new Vector2(speed, 0);
+		} else {
+			timeElapsed += delta;
+			backgroundImage.Position += new Vector2((float)Math.Cos(timeElapsed) * speed, 0);
+			wheel.RotationDegrees = Mathf.Clamp(wheel.RotationDegrees - (float)Math.Cos(timeElapsed + 1.0) * rotationSpeed, minWheelRotation, maxWheelRotation);
+		}
+
 	}
 
 	public void ReceiveData(Dictionary data) {
