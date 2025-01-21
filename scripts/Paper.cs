06851429e8f3d7c53687211dc095833ccb1fbb26
Alec Bargher <abargher@uchicago.edu>
2025-01-20 19:11:35 -0600

    alec: randomized interactable paper scene

---
+++ scripts/Paper.cs
@@ -0,0 +1,40 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class Paper : TextureButton
+{
+	Array<ColorRect> paperSquares = new Array<ColorRect>();
+
+	Random random = new Random();
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// collect all squares
+		for (int i = 1; i <= 49; i++) {
+			paperSquares.Add(GetNode<ColorRect>($"ColorRect/VBoxContainer/HBoxContainer/GridContainer/ButtonBorder{i}"));
+		}
+
+		// disable some squares
+		int numDisabledSquares = random.Next(1, 16);
+		for (int i = 0; i < numDisabledSquares; i++) {
+			int randomIndex = random.Next(0, paperSquares.Count);
+			SetSquareBlack(paperSquares[randomIndex]);
+		}
+
+		foreach (ColorRect square in paperSquares) {
+			square.GetNode<Button>("TextureRect/Button");
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void SetSquareBlack(ColorRect square)
+	{
+		square.GetNode<TextureRect>("TextureRect").Modulate = new Color(0, 0, 0);
+		square.GetNode<Button>("TextureRect/Button").Disabled = true;
+	}
+}
